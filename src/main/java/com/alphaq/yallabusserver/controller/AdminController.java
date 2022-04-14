836da02972d79683f88edfb1fa2277f71fa6de95
MahmoudELwasef <mahmoud.elwsef.0.0@gmail.com>
2022-04-14 21:18:20 +0200

    Test

--- src/main/java/com/alphaq/yallabusserver/controller/AdminController.java
+++ src/main/java/com/alphaq/yallabusserver/controller/AdminController.java
@@ -4,6 +4,7 @@ import com.alphaq.yallabusserver.dao.AdminDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
+//Hello
 @RestController
 public class AdminController {
 
