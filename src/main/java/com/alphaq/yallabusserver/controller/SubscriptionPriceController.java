5bc1471fe16c97d90c9aa97d06d70ccaf8acc176
DESKTOP-IU5JRJJ\Moustafa <moustafa.ibrahem56@gmail.com>
2022-04-15 02:36:38 +0200

    implement SubscriptionPrice Repository,DAO and Controller

--- src/main/java/com/alphaq/yallabusserver/controller/SubscriptionPriceController.java
+++ src/main/java/com/alphaq/yallabusserver/controller/SubscriptionPriceController.java
@@ -1,12 +1,47 @@
 package com.alphaq.yallabusserver.controller;
 
+import com.alphaq.yallabusserver.dao.CompanyDAO;
 import com.alphaq.yallabusserver.dao.SubscriptionPriceDAO;
+import com.alphaq.yallabusserver.dto.SubscriptionPriceDTO;
+import com.alphaq.yallabusserver.entity.Company;
+import com.alphaq.yallabusserver.entity.SubscriptionPrice;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@CrossOrigin(origins = "*")
 @RestController
 public class SubscriptionPriceController {
 
     @Autowired
-    SubscriptionPriceDAO subscriptionPriceDAO;
+    private SubscriptionPriceDAO subscriptionPriceDAO;
+    @Autowired
+    private CompanyDAO companyDAO;
+
+    @GetMapping("/subscriptionPrice/get-all")
+    public List<SubscriptionPrice> getAllSubscriptionPrices() {
+        return subscriptionPriceDAO.getAllSubscriptionPrices();
+    }
+
+    @RequestMapping(value = "/subscriptionPrice/get-by-company-id", method = RequestMethod.GET)
+    public List<SubscriptionPrice> getSubscriptionPricesByCompanyId(@RequestParam("id") int companyId) {
+        return subscriptionPriceDAO.getSubscriptionPricesByCompanyId(companyId);
+    }
+
+    @RequestMapping(value = "/subscriptionPrice/get-by-id", method = RequestMethod.GET)
+    public SubscriptionPrice getSubscriptionPriceById(@RequestParam("id") int subscriptionPriceId) {
+        return subscriptionPriceDAO.getSubscriptionPriceById(subscriptionPriceId);
+    }
+
+    @PostMapping("/subscriptionPrice/save-subscriptionPrice")
+    public SubscriptionPrice save(@RequestBody SubscriptionPriceDTO subscriptionPriceDTO) {
+        SubscriptionPrice subscriptionPrice = new SubscriptionPrice();
+
+        Company company = companyDAO.getCompanyById(subscriptionPriceDTO.getCompany().getId());
+        subscriptionPrice.setCompany(company);
+        return subscriptionPriceDAO.save(subscriptionPrice);
+    }
+
+
 }
--- src/main/java/com/alphaq/yallabusserver/repository/SubscriptionPriceRepository.java
+++ src/main/java/com/alphaq/yallabusserver/repository/SubscriptionPriceRepository.java
@@ -4,7 +4,15 @@ import com.alphaq.yallabusserver.entity.SubscriptionPrice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface SubscriptionPriceRepository extends JpaRepository<SubscriptionPrice,Integer> {
+public interface SubscriptionPriceRepository extends JpaRepository<SubscriptionPrice, Integer> {
+
+    SubscriptionPrice findSubscriptionPriceById(int subscriptionPriceId);
+
+    SubscriptionPrice findSubscriptionPriceByIdAndCompanyId(int subscriptionPriceId, int companyId);
+
+    List<SubscriptionPrice> findSubscriptionPricesByCompanyId(int companyId);
 
 }
