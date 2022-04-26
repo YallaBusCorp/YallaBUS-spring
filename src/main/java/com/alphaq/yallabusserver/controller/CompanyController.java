c188635400a9db9062bad6c9b1139ee0cb928317
MahmoudELwasef <mahmoud.elwsef.0.0@gmail.com>
2022-04-26 02:45:08 +0200

    update company api name from company to companyy to test Azure Host

--- src/main/java/com/alphaq/yallabusserver/controller/CompanyController.java
+++ src/main/java/com/alphaq/yallabusserver/controller/CompanyController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @CrossOrigin(origins = "*")
 @RestController
-@RequestMapping("api/v1/company")
+@RequestMapping("api/v1/companyy")
 public class CompanyController {
 
     @Autowired
